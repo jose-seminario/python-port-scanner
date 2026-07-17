bb55826dc76e4fe1f24956e987f7dce7ed120ce1
Jose Seminario <chuquipoma123@gmail.com>
2026-07-17 15:59:55 -0500

    Implement basic socket connection using user input

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-print("=" * 40)
-print("      Python Port Scanner")
-print("=" * 40)
+import socket
+
+print("*" * 42)
+print("         Python Port Scanner")
+print("*" * 42)
+
+ip = input("Enter IP: ")
+puerto = input("Enter Port: ")
+
+scanner = socket.socket()
+
+try:
+    scanner.connect((ip, int(puerto)))
+    print("CONNECTED")
+except:
+    print("CLOSED")
