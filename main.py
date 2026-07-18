77d134b6db209ef7d194ccad0cf9977f3f158809
Jose Seminario <chuquipoma123@gmail.com>
2026-07-18 15:15:54 -0500

    Add robust port validation

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-import socket
+def scanner(port):
+    print(f"Escaneando el puerto {port}...")
 
 print("*" * 42)
 print("         Python Port Scanner")
 print("*" * 42)
 
-ip = input("Enter IP: ")
-puerto = input("Enter Port: ")
+puerto_valido = False
 
-scanner = socket.socket()
+while puerto_valido == False:
+    try:
+        puerto = input("Enter Port: ")
+        puerto = int(puerto)
 
-try:
-    scanner.connect((ip, int(puerto)))
-    print("CONNECTED")
-except:
-    print("CLOSED")
-    
+    except:
+        print("Inserte un número válido")
+        continue
+
+    if puerto >= 0 and puerto <= 65535:
+        puerto_valido = True
+    else:
+        print("Ingrese un puerto entre 0 y 65535")
+
+scanner(puerto)
