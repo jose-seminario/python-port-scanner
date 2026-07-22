6af46c7ecb0365378d28125bbd7e179f06624c35
Jose Seminario <chuquipoma123@gmail.com>
2026-07-22 18:50:13 -0500

    Refresh, organized

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 
+
 def scanner(ip, port):
     print(f"Scanning port {port}...")
 
@@ -15,20 +16,22 @@ def scanner(ip, port):
     finally:
         connection.close()
 
-def show_banner():
 
+def show_banner():
     print("*" * 42)
     print("         Python Port Scanner")
     print("*" * 42)
+
 
 def show_menu():
     print("1. Scan a specific port or a port range")
     print("2. Scan all ports")
 
+
 def get_user_option():
     valid_option = False
 
-    while valid_option == False:
+    while not valid_option:
         try:
             user_option = int(input("Choose an option: "))
 
@@ -43,21 +46,37 @@ def get_user_option():
 
     return user_option
 
-valid_port = False
 
-ip = input("Enter IP address or domain: ")
+def get_ip():
+    ip = input("Enter IP address or domain: ")
+    return ip
 
-while valid_port == False:
-    try:
-        port = int(input("Enter port: "))
 
-    except:
-        print("Please enter a valid number.")
-        continue
+def get_port():
+    valid_port = False
 
-    if 0 <= port <= 65535:
-        valid_port = True
-    else:
-        print("Please enter a port between 0 and 65535.")
+    while not valid_port:
+        try:
+            port = int(input("Enter port: "))
+
+        except:
+            print("Please enter a valid number.")
+            continue
+
+        if 0 <= port <= 65535:
+            valid_port = True
+        else:
+            print("Please enter a port between 0 and 65535.")
+
+    return port
+
+
+show_banner()
+show_menu()
+
+option = get_user_option()
+
+ip = get_ip()
+port = get_port()
 
 scanner(ip, port)
