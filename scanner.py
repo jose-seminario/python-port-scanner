2e58007b9a803f2e29842298f60a378a1e87d882
Jose Seminario <chuquipoma123@gmail.com>
2026-07-23 22:36:39 +0000

    Add service name detection

--- scanner.py
+++ scanner.py
@@ -15,8 +15,15 @@ def scanner(ip, port):
     finally:
         connection.close()
 
+def get_service(port):
+    try:
+        return socket.getservbyport(port)
+    except:
+        return "Unknown"
 
 def scan_port_range(ip, start_port, end_port):
     for port in range(start_port, end_port + 1):
         if scanner(ip, port):
-            print(f"[OPEN] Port {port}")
+            service = get_service(port)
+            print(f"[OPEN] Port {port} - {service}")
+            
